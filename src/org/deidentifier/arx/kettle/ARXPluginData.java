eee23c58513616f9943df0c421bc559781e90c60
WiednerF <florian.wiedner@tum.de>
2016-05-12 13:15:57 +0200

    Added JavaDoc of the ARXPluginData

--- src/org/deidentifier/arx/kettle/ARXPluginData.java
+++ src/org/deidentifier/arx/kettle/ARXPluginData.java
@@ -1,9 +1,20 @@
-package org.deidentifier.arx.kettle;
 /*
- *
- *
+ * Plugin for Kettle with ARX: Powerful Data Anonymization
+ * Copyright 2016 Florian Wiedner and contributors
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
+package org.deidentifier.arx.kettle;
 
 
 import org.pentaho.di.core.row.RowMetaInterface;
@@ -11,15 +22,24 @@ import org.pentaho.di.trans.step.BaseStepData;
 import org.pentaho.di.trans.step.StepDataInterface;
 
 /**
- * 
- * 
- * @author Matt
- * @since  24-mrt-2005
+ *This class is a Container for the Data and the RowMeta output of this step
+ * @author Florian Wiedner
+ * @since 1.7
+ * @category StepDataInterface
+ * @version 1.0
  */
 public class ARXPluginData extends BaseStepData implements StepDataInterface
 {
+	/**
+	 * The Row Meta Interface Output
+	 */
 	public RowMetaInterface outputRowMeta;
 
+	/**
+	 * Generates the Data and the BaseStepData
+	 * @author Florian Wiedner
+	 * @since 1.7
+	 */
     public ARXPluginData()
 	{
 		super();
